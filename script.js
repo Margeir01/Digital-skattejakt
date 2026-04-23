bfa2df9d29d410ee4ab2a2725b99a24a215a890f
margeir01 <margeir.oddgeirsson@gmail.com>
2026-04-23 10:09:13 +0200

    oppdatering

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function login() {
     setTimeout(() => {
       window.location.href = "admin.html";
     }, 900);
-  } else {
+  } else {git
     statusText.textContent = "Feil brukernavn eller passord.";
     // Shake animation on error
     const card = document.querySelector(".card");
